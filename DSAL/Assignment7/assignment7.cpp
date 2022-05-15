cb9bf050a79648f00e9afc80d5d91ab5fdc9e61b
Sharvi Endait <sharviendait@gmail.com>
2022-05-16 00:37:48 +0530

    prims algo added

--- DSAL/Assignment7/assignment7.cpp
+++ DSAL/Assignment7/assignment7.cpp
@@ -1,59 +1,156 @@
-//============================================================================
-// Name        : Assignment7.cpp
-// Author      : Sharvi
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*
+You have a business with several offices; you want to lease phone lines to connect
+them up with each other; and the phone company charges different amounts of
+money to connect different pairs of cities. You want a set of lines that connects all
+your offices with a minimum total cost. Solve the problem by suggesting appropriate
+data structures.
+*/
 
 #include<iostream>
 using namespace std;
-#define INT_MAX 2147483647
-class Graph{
-    int vernum;
-    int edgenum;
-    int **adj ;
-    
-    public:
-    Graph()
-    {
-        vernum=0;
-        edgenum=0;
-        adj=nullptr;
-    }
-    Graph(int v,int e)
-    {
-        vernum=v;
-        edgenum=e;
-        adj=new int *[v];
-        for(int i=0;i<v;i++)
-        {
-        	adj[i]=new int[v];
-        	for(int j=0;j<v;j++)
-        	{
-        		adj[i][j]=0;//to avoid storing garbage values in the matrix
-        	}
-        }
-        
-    }
-    void display();
-    void acceptgraph();
-    void prims();
-};
-void Graph::acceptgraph()
+class Graph
 {
-    int s,d;
-    int w;
-    for(int i=0;i<edgenum;i++)
+   int a[10][10];
+   int vertices;
+   int edges;
+   int weight;
+
+   void addEdge(int u, int v, int w)
+   {
+      a[u][v] = w;
+      a[v][u] = w;
+   }
+
+   public:
+   Graph(int v,int e)
+   {
+        vertices = v;
+        edges = e;
+        weight = 0;
+        for(int i=0;i<vertices;i++)
+        {
+             for(int j=0;j<vertices;j++)
+             {
+                a[i][j] = 0;
+             }
+        }
+   }
+   void displaygraph();
+   void creategraph();
+   void prims();
+};
+
+void Graph::creategraph()
+{
+    int source,destination,weight;
+    for(int i=0;i<edges;i++)
     {
-        cout<<"Enter the source vertex: ";
-        cin>>s;
-        cout<<"Enter the destination vertex: ";
-        cin>>d;
+        cout<<"Enter the source: ";
+        cin>>source;
+        cout<<"Enter the destination: ";
+        cin>>destination;
         cout<<"Enter the weight: ";
-        cin>>w;
-        adj[s][d]=w;
-        adj[d][s]=w;     
+        cin>>weight;
+        addEdge(source,destination,weight);
+    }
+}
+void Graph::displaygraph()
+{
+    for(int i=0;i<vertices;i++)
+    {
+        for(int j=0;j<vertices;j++)
+        {
+            cout<<i<<"--"<<j<<":"<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+void Graph::prims()
+{
+    int min,i,j,k,min_index,u,v;
+    int visited[10];
+    int distance[10];
+    for(i=0;i<vertices;i++)
+    {
+        visited[i] = 0;
+        distance[i] = a[0][i];
+    }
+    visited[0] = 1;
+    distance[0] = 0;
+    for(i=1;i<vertices;i++)
+    {
+        min = 99999;
+        for(j=0;j<vertices;j++)
+        {
+            if(visited[j] == 0 && distance[j] < min)
+            {
+                min = distance[j];
+                min_index = j;
+            }
+        }
+        visited[min_index] = 1;
+        for(k=0;k<vertices;k++)
+        {
+            if(visited[k] == 0 && a[min_index][k] < distance[k])
+            {
+                distance[k] = a[min_index][k];
+            }
+        }
+    }
+    for(i=0;i<vertices;i++)
+    {
+        for(j=0;j<vertices;j++)
+        {
+            cout<<i<<"--"<<j<<":"<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"The minimum spanning tree is: "<<endl;
+    for(i=0;i<vertices;i++)
+    {
+        for(j=0;j<vertices;j++)
+        {
+            if(a[i][j] != 0)
+            {
+                cout<<i<<"--"<<j<<":"<<a[i][j]<<" ";
+            }
+        }
+        cout<<endl;
     }
 }
 
+int main()
+{
+    int vertices,edges;
+    cout<<"Enter the number of vertices: ";
+    cin>>vertices;
+    cout<<"Enter the number of edges: ";
+    cin>>edges;
+    int ch;
+    Graph g(vertices,edges);
+    do
+    {
+        cout<<"1.Create graph"<<endl;
+        cout<<"2.Display graph"<<endl;
+        cout<<"3.Prims algorithm"<<endl;
+        cout<<"4.Exit"<<endl;
+        cout<<"Enter your choice: ";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+                g.creategraph();
+                break;
+            case 2:
+                g.displaygraph();
+                break;
+            case 3:
+                g.prims();
+                break;
+            case 4:
+                break;
+            default:
+                cout<<"Invalid choice"<<endl;
+        }
+}while(ch!=4);
+}
